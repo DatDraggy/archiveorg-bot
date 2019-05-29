301ede3a485a6235eb0ea50b60b1493dd7c22762
DatDraggy <spamme@kieran.de>
2019-05-29 15:05:17 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -9,6 +9,22 @@ $response = file_get_contents('php://input');
 $data = json_decode($response, true);
 $dump = print_r($data, true);
 $chatId = $data['message']['chat']['id'];
-$chatType = $data['message']['chat']['type'];
+if (isset($data['message']['text'])) {
+  $text = $data['message']['text'];
+} else if (isset($data['message']['caption'])) {
+  $text = $data['message']['caption'];
+}
 
-sendMessage($chatId, 'Test');
+if (!empty($data['message']['entities'])) {
+  foreach ($data['message']['entities'] as $entity) {
+    if ($entity['type'] == 'url') {
+      mail($config['mail'], 'Debug', mb_substr($text, $entity['offset'], $entity['length']));
+    }
+  }
+} else if (!empty($data['message']['caption_entities'])) {
+  foreach ($data['message']['caption_entities'] as $entity) {
+    if ($entity['type'] == 'url') {
+      mail($config['mail'], 'Debug', mb_substr($text, $entity['offset'], $entity['length']));
+    }
+  }
+}
