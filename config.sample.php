bc661109e91d1bd56501b89e60820ce0845506ef
Kieran <bmxstreet@gmx.net>
2019-05-30 11:06:23 +0200

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -2,3 +2,5 @@
 $config['token'] = '123456:awrgojndrgojn';
 $config['url'] = 'https://api.telegram.org/bot' . $config['token'] . '/';
 $config['mail'] = 'telegram@example.com';
+$config['savedIn] = '/var/www/images/';
+$config['mediaPath'] = '';
