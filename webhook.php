524257c47adab84bf412bcf848c157607f902716
DatDraggy <spamme@kieran.de>
2019-06-03 12:09:44 +0200

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,7 +1,7 @@
 <?php
 require_once(__DIR__ . '/funcs.php');
 require_once(__DIR__ . '/config.php');
-require_once(__DIR__ . '/vendor/autoload.php');
+require_once('/var/libraries/composer/vendor/autoload.php');
 
 $response = file_get_contents('php://input');
 $data = json_decode($response, true);
